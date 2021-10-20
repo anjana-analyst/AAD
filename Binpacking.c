2f1fdae6f5a218bc4bdda897843f2159fe19dd93
anjana-analyst <65863923+anjana-analyst@users.noreply.github.com>
2021-10-20 11:04:55 +0530

    Create Binpacking.c

---
+++ Binpacking.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int bins(int n , int a[] , int capacity)
+{
+    int count=1,k = capacity;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i] < capacity)
+        {
+          capacity-=a[i];
+        }
+        else
+        {
+          count++;
+          k = capacity;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    int capacity;
+    scanf("%d",&capacity);
+    printf("%d" , bins(n,a,capacity));
+    return 0;
+}
